18ca1220d3861d04521adc5e5569d022488c3777
spshanmugamurthi <spshanmugamurthi@gmail.com>
2018-02-23 10:54:24 +0530

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :todos, foreign_key: :user_id, dependent: :destroy
   validates_presence_of :name, :email, :password_digest
+  validates_uniqueness_of :email
 end
